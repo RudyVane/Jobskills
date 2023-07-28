f6a5c9e2bcb29b009d066c85566c922e07a6961b
Jeroen van Leusen <Kiskae@users.noreply.github.com>
2023-07-28 14:00:19 +0000

    turn discord module into blueprint

--- jobskills/__init__.py
+++ jobskills/__init__.py
@@ -2,8 +2,10 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
-from .discord.flask import setup as discord_setup
+from .discord.flask import blueprint as discord_blueprint
+
 # from .scrape import scrape
+
 
 def main():
     load_dotenv()
@@ -13,32 +15,33 @@ def main():
     app = Flask(__name__)
     app.config.from_prefixed_env()
 
+    app.register_blueprint(discord_blueprint)
+
     @app.route("/")
     def hello():
         return "Hello World"
-    
-    if 'API_KEY' in os.environ:
+
+    if "API_KEY" in os.environ:
         from api_test.gpt import api_interaction
-    
+
         @app.route("/test")
         def gpttest():
             skills_matrix_file = "./api_test/skills_matrix.txt"
             job_advert_file = "./api_test/job_advert.txt"
             result = api_interaction(skills_matrix_file, job_advert_file)
             return result
-    
-    #Todo:
+
+    # Todo:
     # -implement functionality in a proper format.
     # -decide file formats if any files are used
-    ''' 
+    """ 
     @app.route("/gpt")
     def gpt():
         skills_matrix_file = "userid/skils_matrix"
         job_advert_file = "url scraped"
         result = api_interaction(skills_matrix_file, job_advert_file)
         return result            
-    '''
-    discord_setup(app)
+    """
 
     print(app.url_map)
 
--- jobskills/discord/flask.py
+++ jobskills/discord/flask.py
@@ -1,19 +1,28 @@
 import os
 
-from flask import Flask
-from flask_discord_interactions import DiscordInteractions
+from flask.blueprints import Blueprint, BlueprintSetupState
 
+from flask_discord_interactions import DiscordInteractions
 from .commands import blueprints
 
-def setup(app: Flask):
-    discord = DiscordInteractions(app)
+blueprint = Blueprint("discord_integration", __name__)
+
+
+def _setup(state: BlueprintSetupState):
+    discord = DiscordInteractions(state.app)
 
     for bp in blueprints:
         discord.register_blueprint(bp)
 
     discord.set_route("/interaction/")
 
+    # TODO: move to option in state.settings
     if "TESTING_GUILD" in os.environ:
         discord.update_commands(guild_id=os.environ["TESTING_GUILD"])
     else:
-        app.logger.warn("Not updating discord commands, no 'TESTING_GUILD' id set")
+        state.app.logger.warn(
+            "Not updating discord commands, no 'TESTING_GUILD' id set"
+        )
+
+
+blueprint.record_once(_setup)
