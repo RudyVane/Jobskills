23f9c3826d81db22136d3e54bfbcaa8624828461
Jeroen van Leusen <Kiskae@users.noreply.github.com>
2023-07-28 12:13:26 +0000

    modal example

--- jobskills/discord/commands/__init__.py
+++ jobskills/discord/commands/__init__.py
@@ -1,4 +1,5 @@
 from .ping import bp as ping_bp
 from .offer import bp as offer_bp
+from .modal import bp as modal_bp
 
-blueprints = [ping_bp, offer_bp]
+blueprints = [ping_bp, offer_bp, modal_bp]
---
+++ jobskills/discord/commands/modal.py
@@ -0,0 +1,18 @@
+from flask_discord_interactions import DiscordInteractionsBlueprint, Modal, ActionRow, TextInput
+
+
+bp = DiscordInteractionsBlueprint()
+
+@bp.command()
+def modal_test(ctx):
+    fields = [
+        ActionRow([
+            TextInput("test_input", "What do you have to say?")
+        ])
+    ]
+    return Modal("modal_test_response", "Hi There", fields)
+
+@bp.custom_handler("modal_test_response")
+def modal_test_response(ctx):
+    input = ctx.get_component("test_input")
+    return f"You told me about {len(input.value)} characters of input"
