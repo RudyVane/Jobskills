d84a136eaf8a63ac8402e7d1758c9df15d9090ae
Rudy Vane <87861321+RudyVane@users.noreply.github.com>
2023-08-04 10:07:43 +0200

    Create help.py

---
+++ jobskills/discord/commands/help.py
@@ -0,0 +1,20 @@
+from flask_discord_interactions import DiscordInteractionsBlueprint
+
+
+bp = DiscordInteractionsBlueprint()
+
+
+@bp.command()
+def help(ctx):
+    return "Welcome to the jobskills-matcher
+
+This app is ment to compare your own skills with the required skills from a job offer text.
+
+To start this app type; /1
+
+The pop-up will ask you to paste your resume or your skillmatrix
+
+Then you give the URL of the job offer
+
+By clicking the OK button the app will compare both inputs and return a list of matching skills 
+and a matching percentage."
