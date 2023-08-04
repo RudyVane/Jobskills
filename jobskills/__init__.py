4a9725ccb207df855666fad83cc70aa1daee2f40
Jeroen van Leusen <Kiskae@users.noreply.github.com>
2023-08-04 15:14:09 +0200

    move app initialization to own module

--- jobskills/__init__.py
+++ jobskills/__init__.py
@@ -12,37 +12,6 @@ def main():
     # as example of how to use the crawler:
     # scrape("https://www.yelgo.nl/vacatures/junior-developer-software/", print)
 
-    app = Flask(__name__)
-    app.config.from_prefixed_env()
-
-    app.register_blueprint(discord_blueprint)
-
-    @app.route("/")
-    def hello():
-        return "Hello World"
-
-    if "API_KEY" in os.environ:
-        from api_test.gpt import api_interaction
-
-        @app.route("/test")
-        def gpttest():
-            skills_matrix_file = "./api_test/skills_matrix.txt"
-            job_advert_file = "./api_test/job_advert.txt"
-            result = api_interaction(skills_matrix_file, job_advert_file)
-            return result
-
-    # Todo:
-    # -implement functionality in a proper format.
-    # -decide file formats if any files are used
-    """ 
-    @app.route("/gpt")
-    def gpt():
-        skills_matrix_file = "userid/skils_matrix"
-        job_advert_file = "url scraped"
-        result = api_interaction(skills_matrix_file, job_advert_file)
-        return result            
-    """
-
-    print(app.url_map)
+    from .flask import app
 
     app.run(host="::", port=8080, debug=True)
---
+++ jobskills/flask.py
@@ -0,0 +1,25 @@
+
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from .discord.flask import blueprint as discord_blueprint
+
+app = Flask(__name__)
+app.config.from_prefixed_env()
+
+app.register_blueprint(discord_blueprint)
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+if "API_KEY" in os.environ:
+    from api_test.gpt import api_interaction
+
+    @app.route("/test")
+    def gpttest():
+        skills_matrix_file = "./api_test/skills_matrix.txt"
+        job_advert_file = "./api_test/job_advert.txt"
+        result = api_interaction(skills_matrix_file, job_advert_file)
+        return result
