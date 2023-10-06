3db8c2119892ccd6d963b937a6f9f770e2f362f5
Kiskae <Kiskae@users.noreply.github.com>
2023-10-06 11:00:53 +0000

    use quart_flask_patch instead of quart.flash_patch

--- src/jobskills/discord/__init__.py
+++ src/jobskills/discord/__init__.py
@@ -3,4 +3,4 @@ Relies on the module loading system importing the parent modules
  before any of the submodules.
 """
 
-import quart.flask_patch  # noqa: F401
+import quart_flask_patch  # noqa: F401
--- tests/test_flask.py
+++ tests/test_flask.py
@@ -2,4 +2,4 @@ def test_flask_patch():
     import jobskills.flask  # isort:skip # noqa: F401
     from flask import request
 
-    assert "quart.flask_patch" in type(request).__module__
+    assert "quart_flask_patch" in type(request).__module__
