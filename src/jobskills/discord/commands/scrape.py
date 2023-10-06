64938e53845ebfa0e19a8a89bb2a06addca5b243
Rick <eralon18@gmail.com>
2023-10-06 11:35:47 +0200

    change /scrape to a modal

--- src/jobskills/discord/commands/scrape.py
+++ src/jobskills/discord/commands/scrape.py
@@ -1,4 +1,12 @@
-from flask_discord_interactions import DiscordInteractionsBlueprint, Message
+from flask_discord_interactions import (
+    ActionRow,
+    DiscordInteractionsBlueprint,
+    Modal,
+    TextInput,
+    TextStyles,
+    FileInput,
+    Message
+)
 
 from jobskills.jobqueue import get_queue
 
@@ -6,7 +14,26 @@ bp = DiscordInteractionsBlueprint()
 
 # TODO allow file upload with url. modal i guess?
 @bp.command()
-async def scrape(ctx, url: str, file: any):
+def scrape(ctx):
+    # Define the modal fields
+    fields = [
+        ActionRow(
+            [
+                TextInput("url_input", "Enter the URL:", required=True),
+                FileInput("file_input", "Upload resume:", required=True)
+            ]
+        )
+    ]
+    # Open the modal
+    return Modal("scrape_modal", "Scrape Data", fields)
+
+@bp.custom_handler("scrape_modal")
+async def scrape_submit(ctx):
+    # Retrieve data from the modal
+    url = ctx.get_component("url_input").value
+    file = ctx.get_component("file_input").value if ctx.get_component("file_input") else None
+
+    # Enqueue the scrape_pipeline job with the provided data
     async with get_queue() as q:
         await q.enqueue_job("scrape_pipeline", ctx.freeze(), url, file)
-    return Message(deferred=True)
+    return Message(content="Processing your request...")
