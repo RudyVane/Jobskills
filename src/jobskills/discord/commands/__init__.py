acf07b7b0ddb45f301ae6f2ae4ac78bb358c9f1b
Kiskae <Kiskae@users.noreply.github.com>
2023-10-06 12:12:46 +0000

    refactor flask intergration, add dynaconf

--- src/jobskills/discord/commands/__init__.py
+++ src/jobskills/discord/commands/__init__.py
@@ -4,3 +4,9 @@ blueprints = [
     importlib.import_module(f".{name}", __name__).bp
     for name in ["ping", "offer", "modal", "help", "scrape"]
 ]
+
+
+def dump():
+    return [
+        command.dump() for bp in blueprints for command in bp.discord_commands.values()
+    ]
--- src/jobskills/discord/flask.py
+++ src/jobskills/discord/flask.py
@@ -1,14 +1,16 @@
 import os
 
+from dynaconf import FlaskDynaconf
 from flask_discord_interactions import DiscordInteractions
+from quart import Quart
 from quart.blueprints import Blueprint, BlueprintSetupState
 
-from .commands import blueprints
-
-blueprint = Blueprint("discord_integration", __name__)
+from ..config import settings
 
 
 def _setup(state: BlueprintSetupState):
+    from .commands import blueprints
+
     discord = DiscordInteractions(state.app)
 
     for bp in blueprints:
@@ -20,15 +22,36 @@ def _setup(state: BlueprintSetupState):
     if "TESTING_GUILD" in os.environ:
         discord.update_commands(guild_id=os.environ["TESTING_GUILD"])
     else:
-        state.app.logger.warn(
+        state.app.logger.warning(
             "Not updating discord commands, no 'TESTING_GUILD' id set"
         )
 
 
-def dump_commands():
-    return [
-        command.dump() for bp in blueprints for command in bp.discord_commands.values()
-    ]
-
-
+blueprint = Blueprint("discord_integration", __name__)
 blueprint.record_once(_setup)
+
+
+def discord_extension(app):
+    app.register_blueprint(blueprint)
+
+
+def make_app():
+    app = Quart(__name__)
+
+    FlaskDynaconf(app, dynaconf_instance=settings, extensions_list="EXTENSIONS")
+
+    @app.route("/")
+    def hello():
+        return "Hello World"
+
+    if "API_KEY" in os.environ:
+        from ..gpt import api_interaction
+
+        @app.route("/test")
+        def gpttest():
+            skills_matrix_file = "./api_test/skills_matrix.txt"
+            job_advert_file = "./api_test/job_advert.txt"
+            result = api_interaction(skills_matrix_file, job_advert_file)
+            return result
+
+    return app
