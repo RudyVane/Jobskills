bc13828b4ec8b8a7d452699fc224dd03791b3f04
Kiskae <Kiskae@users.noreply.github.com>
2023-10-13 08:55:17 +0000

    add validation for discord secrets

--- src/jobskills/discord/__init__.py
+++ src/jobskills/discord/__init__.py
@@ -4,3 +4,15 @@ Relies on the module loading system importing the parent modules
 """
 
 import quart_flask_patch  # noqa: F401
+from dynaconf import Validator
+
+from ..config import settings
+
+settings.validators.register(
+    Validator("DISCORD_CLIENT_ID", must_exist=True),
+    Validator("DISCORD_CLIENT_SECRET", must_exist=True),
+    # 32-byte public key, 64 characters as hex
+    Validator("DISCORD_PUBLIC_KEY", must_exist=True, len_eq=64),
+    Validator("discord.testing_guild_id"),
+    Validator("discord.endpoint"),
+)
