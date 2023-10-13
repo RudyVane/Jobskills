67190466ba6c08b8b07c0cd7531b6356c99e0d9a
Kiskae <Kiskae@users.noreply.github.com>
2023-10-13 08:56:19 +0000

    importing `jobskills.flask` without secrets causes an exception.

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,5 +1,5 @@
 def test_flask_patch():
-    import jobskills.flask  # isort:skip # noqa: F401
+    import jobskills.discord.flask  # isort:skip # noqa: F401
     from flask import request
 
     assert "quart_flask_patch" in type(request).__module__
