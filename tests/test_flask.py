fa08c9cc4862775f3feaf92993b9591053fcc9f8
Kiskae <Kiskae@users.noreply.github.com>
2023-11-10 08:54:39 +0000

    add test for asyncio functionality

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,5 +1,14 @@
+import jobskills.discord.flask  # noqa: F401
+
+
 def test_flask_patch():
-    import jobskills.discord.flask  # isort:skip # noqa: F401
     from flask import request
 
     assert "quart_flask_patch" in type(request).__module__
+
+
+def test_async_works():
+    from flask_discord_interactions import DiscordInteractions
+    from quart import Quart
+
+    DiscordInteractions().set_route_async("/test/", app=Quart(__name__))
